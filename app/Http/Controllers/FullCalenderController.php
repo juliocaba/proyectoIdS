0bf10dbea9fbbc149ce83ee8f538f6faa131afea
fgmfgm <101155130+fgmfgm@users.noreply.github.com>
2022-05-01 14:42:49 -0300

    Revert "Bueh"
    This reverts commit ed16b535c65b9e35bc3f36808bb8d6126c15b80f.

--- app/Http/Controllers/FullCalenderController.php
+++ app/Http/Controllers/FullCalenderController.php
@@ -25,7 +25,7 @@ class FullCalenderController extends Controller
             $date->end = (new DateTime($date->end))->format(DateTime::ISO8601);
         }
 
-        return response()->json($data);
+        return response()->json($res);
     }
  
     /**

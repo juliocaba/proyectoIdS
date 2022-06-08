63ff5fb47887945e4082221147309daf17e3452b
leandrocentelles <leandrocentelles@gmail.com>
2022-06-08 19:25:53 -0300

    .

--- resources/views/admin/clientes/show.blade.php
+++ resources/views/admin/clientes/show.blade.php
@@ -5,12 +5,12 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Cliente Details</h1>
+                    <h1>Detalles del cliente</h1>
                 </div>
                 <div class="col-sm-6">
                     <a class="btn btn-default float-right"
                        href="{{ route('admin.clientes.index') }}">
-                        Back
+                        Volver
                     </a>
                 </div>
             </div>
